cf6a0061ce395f9c8426746ac9de522b6933ad26
ajibadeoj <ajibadeoj@gmail.com>
2020-07-30 14:56:02 +0100

    trial on question 1

---
+++ question1.js
@@ -0,0 +1,18 @@
+const name = ["seun", 'shola', 'segun']
+console.log(name)
+
+var vehicle =   {name:['honda', 'toyota', 'ford'],
+                type:['wagon', 'coupe', 'saloon', 'bus'],
+                model:[2013, 2014, 2015, 2016, 2017]
+                }
+console.log(vehicle.model)
+
+const matrix = [
+    ['red', 'blue', 'green'],
+    ['black', 'white', 'purple'],
+    ['orange', 'brown', 'pink']
+]
+console.log("Colour " + matrix[2][1] + " has been selected")
+//brown should be printed
+console.log("Colour " + matrix[1][2] + " has been selected")
+//purple should be printed
